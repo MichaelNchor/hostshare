f207d96366cc49a1f04e8741c69396014cccf6c1
manchor127 <manchor127@gmail.com>
2023-05-30 13:51:06 +0000

    updated

--- src/components/SubNav.js
+++ src/components/SubNav.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useRef, useState} from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
 import {
@@ -257,6 +257,9 @@ export default function SubNav({categories}) {
 
  const Pages = categories;
 
+ const btn_left_el = useRef(null);
+ const btn_right_el = useRef(null);
+ const content_el = useRef(null);
 
   const [ishovered, setHovered] = useState({});
 
@@ -268,22 +271,102 @@ export default function SubNav({categories}) {
     setHovered({});
   }
 
+  const scroll = () => {
+      const   btn_left = document.getElementById('btn_left'),
+              btn_right = document.getElementById('btn_right'),
+              content = document.getElementById('con');
+      const content_scroll_width = content.scrollWidth;
+      let content_scoll_left = content.scrollLeft;
+      btn_right.addEventListener('click', () => {
+          content_scoll_left += 100;
+          if (content_scoll_left >= content_scroll_width) { content_scoll_left = content_scroll_width; }
+          content.scrollLeft = content_scoll_left;
+      });
+      btn_left.addEventListener('click', () => {
+          content_scoll_left -= 100;
+          if (content_scoll_left <= 0) {
+              content_scoll_left = 0;
+          }
+          content.scrollLeft = content_scoll_left;
+      });
+  }
+
   const styles = [
-    {text: 'text-xs my-2 text-center w-24 text-center m-2', bar:'bg-black h-[2px]'},
-    {text: 'text-xs my-2 text-center text-gray-400 w-24 text-center m-2', bar: 'bg-gray-400 h-[2px]'},
+    {text: 'text-[10pt] my-2 text-center w-24 text-center m-2', bar:'bg-black h-[2px]'},
+    {text: 'text-[10pt] my-2 text-center text-gray-400 w-24 text-center m-2', bar: 'bg-gray-400 h-[2px]'},
   ]
 
   return (
     <>
-    <div className='z-[29] top-10 px-12 bg-white no-scrollbar sticky overflow-x-auto lg:top-[6.95rem] flex lg:px-12 justify-center items-center'>
+    <div className='flex px-12'>
+    <div className='w-14 flex justify-center items-center'>
+    <button
+        type="button"
+        className="left-0 z-[30] pr-1 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        data-carousel-prev=""
+        onClick={() => scroll('left')}
+        ref={btn_left_el}
+        id="btn_left"
+    >
+        <span className="inline-flex items-center justify-center w-4 h-4 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-100/30 group-hover:bg-white/50 dark:group-hover:bg-gray-200/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none  border-[1px] border-gray-600">
+        <svg
+            aria-hidden="true"
+            className="w-4 h-4 text-gray-900 sm:w-5 sm:h-5 dark:text-white"
+            fill="none"
+            stroke="#000000"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M15 19l-7-7 7-7"
+            />
+        </svg>
+        <span className="sr-only">Previous</span>
+        </span>
+    </button> 
+    </div>
+    <div id='con'  className='child z-[29] top-10 px-12 bg-white no-scrollbar sticky overflow-x-auto lg:top-[6.95rem] flex lg:px-12 justify-center items-center'>
     {Pages.map(function(p){
     return <Link  key={p["id"]} href={`/listings/${p["type"]}`} className="flex items-center justify-center"><div onMouseOut={()=>isOut(p)} onMouseOver={()=>isHovered(p)} className='flex flex-col my-7 mx-6 cursor-pointer'>
-    <Image src={p["title"] == ishovered["title"]?home:home_gray} width="40" height="40" alt={p["title"]} className='mx-auto grayscale' />
+    {/* <Image src={p["title"] == ishovered["title"]?home:home_gray} width="40" height="40" alt={p["title"]} className='mx-auto grayscale' /> */}
     <span className={p["title"] == ishovered["title"]?styles[0].text:styles[1].text}>{p["title"]}</span>
     <div className={p["title"] == ishovered["title"]?styles[0].bar:styles[1].bar}></div>
     </div>
     </Link>
     })}
+  </div>
+  <div className='w-14 flex justify-center items-center'>
+  <button
+        type="button"
+        className="left-0 z-[30] pl-1 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        data-carousel-prev=""
+        onClick={() => scroll('right')}
+        ref={btn_right_el}
+        id="btn_right"
+    >
+        <span className="inline-flex items-center justify-center w-4 h-4 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-100/30 group-hover:bg-white/50 dark:group-hover:bg-gray-200/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none  border-[1px] border-gray-600">
+        <svg
+            aria-hidden="true"
+            className="w-4 h-4 text-gray-900 sm:w-5 sm:h-5 dark:text-white"
+            fill="none"
+            stroke="#000000"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9 5l7 7-7 7"
+            />
+        </svg>
+        <span className="sr-only">Next</span>
+        </span>
+    </button>
+  </div>
   </div>
     </>
   )
